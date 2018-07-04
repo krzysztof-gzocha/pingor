ff29951fcfd861c5c7478f2e1001e35176ea35dd
Krzysztof Gzocha <krzysztof@propertyfinder.ae>
2018-07-04 17:43:30 +0200

    Small fixes

--- pkg/persister/aws/dynamodb/marshaller.go
+++ pkg/persister/aws/dynamodb/marshaller.go
@@ -5,12 +5,15 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
+// MarshallerInterface is an interface helping with testing dynamo db persister
 type MarshallerInterface interface {
 	MarshalMap(in interface{}) (map[string]*dynamodb.AttributeValue, error)
 }
 
+// Mershaller will re-use dynamodbattribute.MarshalMap function. It's just hidden within the struct for easier testing
 type Marshaller struct{}
 
+// MarshalMap will call dynamodbattribute.MarshalMap
 func (m Marshaller) MarshalMap(in interface{}) (map[string]*dynamodb.AttributeValue, error) {
 	return dynamodbattribute.MarshalMap(in)
 }
